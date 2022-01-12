95ff90f3e0cfec3592e15228e33dc92e3e881ff1
benjaminlangenaken <benjamin.langenaken@outlook.com>
2022-01-12 14:31:38 +0100

    Add files, collection js

---
+++ collection.js
@@ -0,0 +1,102 @@
+const albumCollection = [
+    {
+        rankNr: 10,
+        name: "The Miseducation of Lauryn Hill",
+        artist: "Lauryn Hill",
+        releaseYear: 1998,
+        genre: ["R&B", "soul"],
+        info: "“This is a very sexist industry,” Lauryn Hill told Essence magazine in 1998. “They’ll never throw the ‘genius’ title to a sister.” Though already a star as co-leader of the Fugees, with Wyclef Jean, she was hungry to express her own vision. “[I wanted to] write songs that lyrically move me and have the integrity of reggae and the knock of hip-hop and the instrumentation of classic soul,” the singer said of her debut album.",
+        imgUrl: "https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-010-Lauryn-Hill-MISEDUCATION.jpg?w=1000",
+        spotifyUrl: "https://open.spotify.com/album/1BZoqf8Zje5nGdwZhOjAtD"
+    },
+    {
+        rankNr: 9,
+        name: "Blood on the Tracks",
+        artist: "Bob Dylan",
+        releaseYear: 1975,
+        genre: ["folk", "rock"],
+        info: "Bob Dylan once introduced this album’s opening song, “Tangled Up in Blue,” onstage as taking him 10 years to live and two years to write. It was, for him, a pointed reference to the personal crisis — the collapse of his marriage to Sara Lowndes — that at least partly inspired this album, Dylan’s best of the 1970s.",
+        imgUrl: "https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-009-Bob-Dylan-BLOOD-ON-THE-TRACKS.jpg?w=1000",
+        spotifyUrl: "https://open.spotify.com/album/4WD4pslu83FF6oMa1e19mF"
+    },
+    {
+        rankNr: 8,
+        name: "Purple Rain",
+        artist: "Prince",
+        releaseYear: 1984,
+        genre: ["funk", "soul"],
+        info: "“I think Purple Rain is the most avant-garde, ‘purple’ thing I’ve ever done,” Prince told Ebony in 1986. He was still a rising star with only a couple of hits when he got the audacious idea to make a movie based on his life, and make his next LP the movie’s soundtrack. When it was released in 1984, he became the first artist to have the Number One song, album, and movie in North America.",
+        imgUrl: "https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-008-Prince-PURPLE-RAIN.jpg?w=1000",
+        spotifyUrl: "https://open.spotify.com/album/7nXJ5k4XgRj5OLg9m8V3zc"
+    },
+    {
+        rankNr: 7,
+        name: "Rumours",
+        artist: "Fleetwood Mac",
+        releaseYear: 1977,
+        genre: ["rock"],
+        info: "With Rumours, Fleetwood Mac turned private turmoil into gleaming, melodic public art. The band’s two couples — bassist John McVie and singer-keyboard player Christine McVie, who were married; guitarist Lindsey Buckingham and vocalist Stevie Nicks, who were not — broke up during the protracted sessions for the album. As John later told Rolling Stone of the atmosphere during the making of Rumours, “Parties going on all over the house. Amazing. Terrifying. Huge amounts of illicit materials, yards and yards of this wretched stuff. Days and nights would just go on and on.”",
+        imgUrl: "https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-007-Fleetwood-Mac-RUMOURS.jpg?w=1000",
+        spotifyUrl: "https://open.spotify.com/album/1bt6q2SruMsBtcerNVtpZB"
+    },
+    {
+        rankNr: 6,
+        name: "Nevermind",
+        artist: "Nirvana",
+        releaseYear: 1991,
+        genre: ["grunge", "alternative"],
+        info: "An overnight-success story of the 1990s, Nirvana’s second album and its totemic ﬁrst single, “Smells Like Teen Spirit,” shot up from the Northwest underground — the nascent grunge scene in Seattle — to kick Michael Jackson’s Dangerous off the top of the Billboard charts and blow hair metal off the map. Few albums have had such an overpowering impact on a generation — a nation of teens suddenly turned punk — and such a catastrophic effect on its main creator. The weight of success led already-troubled singer-guitarist Kurt Cobain to take his own life in 1994.",
+        imgUrl: "https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-006-Nirvana-NEVERMIND-HR.jpg?w=1000",
+        spotifyUrl: "https://open.spotify.com/album/2UJcKiJxNryhL050F5Z1Fk"
+    },
+    {
+        rankNr: 5,
+        name: "Abbey Road",
+        artist: "The Beatles",
+        releaseYear: 1969,
+        genre: ["pop", "rock"],
+        info: "“It was a very happy record,” said producer George Martin, describing this album in The Beatles Anthology. “I guess it was happy because everybody thought it was going to be the last.” Indeed, Abbey Road — recorded in two months during the summer of 1969 — almost never got made at all. That January, the Beatles were on the verge of a breakup, exhausted and angry with one another after the disastrous sessions for the aborted Get Back LP, later salvaged as Let It Be [see No. 342]. Yet determined to go out with the same glory with which they had ﬁrst entranced the world at the start of the decade, the group reconvened at EMI’s Abbey Road Studios to make their most polished album: a collection of superb songs cut with an attention to reﬁned detail, then segued together (especially on Side Two) with conceptual force.",
+        imgUrl: "https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-005-Beatles-ABBEY-ROAD.jpg?w=1000",
+        spotifyUrl: "https://open.spotify.com/album/0ETFjACtuP2ADo6LFhL6HN"
+    },
+    {
+        rankNr: 4,
+        name: "Songs in the Key of Life",
+        artist: "Stevie Wonder",
+        releaseYear: 1976,
+        genre: ["R&B", "soul"],
+        info: "Months before the recording sessions for Songs in the Key of Life ended, the musicians in Stevie Wonder’s band had T-shirts made up that proclaimed, “We’re almost finished.” It was the stock answer to casual fans and Motown executives and everybody who’d fallen in love with Wonder’s early-Seventies gems – 1972’s Talking Book, 1973’s Innervisions, and 1974’s Fulfillingness’ First Finale – and who had been waiting two years for the next chapter. “I believed there was a lot that needed to be said,” Wonder said. More, in fact, than he could fit onto a double album – also included was a bonus EP, a seven-inch single with four more songs from the sessions.",
+        imgUrl: "https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-004-Stevie-Wonder-SONGS-IN-THE-KEY-OF-LIFE.jpg?w=1000",
+        spotifyUrl: "https://open.spotify.com/album/6YUCc2RiXcEKS9ibuZxjt0"
+    },
+    {
+        rankNr: 3,
+        name: "Blue",
+        artist: "Joni Mitchell",
+        releaseYear: 1971,
+        genre: ["folk"],
+        info: "In 1971, Joni Mitchell represented the West Coast feminine ideal — celebrated by Robert Plant as “a girl out there with love in her eyes and flowers in her hair” on Led Zeppelin’s “Goin’ to California.” It was a status that Mitchell hadn’t asked for and did not want: “I went, ‘Oh, my God, a lot of people are listening to me,’” she recalled in 2013. “’They better find out who they’re worshiping. Let’s see if they can take it. Let’s get real.’ So I wrote Blue.”",
+        imgUrl: "https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-003-JoniMitchell-BLUE-HR.jpg?w=1000",
+        spotifyUrl: "https://open.spotify.com/album/1vz94WpXDVYIEGja8cjFNa"
+    },
+    {
+        rankNr: 2,
+        name: "Pet Sounds",
+        artist: "The Beach Boys",
+        releaseYear: 1966,
+        genre: ["surf", "pop"],
+        info: "“Who’s gonna hear this shit?” Beach Boys singer Mike Love asked the band’s resident genius, Brian Wilson, in 1966, as Wilson played him the new songs he was working on. “The ears of a dog?” Confronted with his bandmate’s contempt, Wilson made lemonade of lemons. “Ironically,” he observed, “Mike’s barb inspired the album’s title.”",
+        imgUrl: "https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-002-Beach-Boys-PET-SOUNDS-update.jpg?w=1000",
+        spotifyUrl: "https://open.spotify.com/album/6GphKx2QAPRoVGWE9D7ou8"
+    },
+    {
+        rankNr: 1,
+        name: "What's Going On",
+        artist: "Marvin Gaye",
+        releaseYear: 1971,
+        genre: ["funk", "soul"],
+        info: "Marvin Gaye’s masterpiece began as a reaction to police brutality. In May 1969, Renaldo “Obie” Benson, the Four Tops’ bass singer, watched TV coverage of hundreds of club-wielding cops breaking up the People’s Park, a protest hub in Berkeley. Aghast at the violence, Benson began to write a song with Motown lyricist Al Cleveland, trying to capture the confusion and pain of the times. He knew he had something big in his nascent version of “What’s Going On,” but the rest of the Four Tops weren’t interested, and Benson’s efforts to get Joan Baez to record it didn’t work out, either.",
+        imgUrl: "https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-001-Marvin-Gaye-WHATS-GOING-ON.jpg?w=1000",
+        spotifyUrl: "https://open.spotify.com/album/2v6ANhWhZBUKkg6pJJBs3B"
+    }
+]
